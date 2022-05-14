375bdfff002cb6c9eb1466db7999740205016ec3
developer833 <byronpantoja@gmail.com>
2022-05-14 22:15:30 +0800

    adding links

--- components/data.ts
+++ components/data.ts
@@ -1,32 +1,32 @@
-export default [
+export const data = [
   {
     id: 1,
     title: "Prepp Photo Link 1",
-    url: "https://preppphoto.com",
+    url: "https://hooks.zapier.com/hooks/catch/1801394/bkjpvf8/",
   },
   {
     id: 2,
     title: "Prepp Photo Link 2",
-    url: "https://preppphoto.com",
+    url: "https://hooks.zapier.com/hooks/catch/1801394/bkjpvpd/",
   },
   {
     id: 3,
     title: "Prepp Photo Link 3",
-    url: "https://preppphoto.com",
+    url: "https://hooks.zapier.com/hooks/catch/1801394/bkjny27/  ",
   },
   {
     id: 4,
     title: "Prepp Photo Link 4",
-    url: "https://preppphoto.com",
+    url: "https://hooks.zapier.com/hooks/catch/1801394/bkjny4e/ ",
   },
   {
     id: 5,
     title: "Prepp Photo Link 5",
-    url: "https://preppphoto.com",
+    url: "https://hooks.zapier.com/hooks/catch/1801394/bkjny1x/ ",
   },
   {
     id: 6,
     title: "Prepp Photo Link 6",
-    url: "https://preppphoto.com",
+    url: "https://hooks.zapier.com/hooks/catch/1801394/bkjnya4/  ",
   },
 ];
